d2ced2993e22ccb027bbe7ab1d93a6caca7c6392
Markiian89 <markijannazarko@gmail.com>
2023-10-31 19:52:17 +0200

    lesson 22 homework was added

--- js/app.js
+++ js/app.js
@@ -100,67 +100,148 @@
 
 // Lesson 21 Локальні та зовнішні змінні, функції
 
-function maxNumber(a, b) {
-    if (a > b) {
-      return a;
-    } else if (b > a) {
-      return b;
-    } else {
-      return "a = b";
-    }
-  }
+// function maxNumber(a, b) {
+//     if (a > b) {
+//       return a;
+//     } else if (b > a) {
+//       return b;
+//     } else {
+//       return "a = b";
+//     }
+//   }
   
-  let a = parseFloat(prompt("Enter first number (a):"));
-  let b = parseFloat(prompt("Enter second number (b):"));
+//   let a = parseFloat(prompt("Enter first number (a):"));
+//   let b = parseFloat(prompt("Enter second number (b):"));
   
-  alert(maxNumber(a, b));
+//   alert(maxNumber(a, b));
 
 
-function reverse(number) {
-    return -number;
-  }
+// function reverse(number) {
+//     return -number;
+//   }
   
-  let inputNumber = parseFloat(prompt("Enter the number"));
-  let reversedNumber = reverse(inputNumber);
-  alert(reversedNumber);
+//   let inputNumber = parseFloat(prompt("Enter the number"));
+//   let reversedNumber = reverse(inputNumber);
+//   alert(reversedNumber);
   
-function three(number, count) {
-    return number + 3 * count;
-    }
+// function three(number, count) {
+//     return number + 3 * count;
+//     }
   
-    let number = parseFloat(prompt("Enter the number:"));
-    let count = parseInt(prompt("Enter the number of additions (count):"));
-    alert(three(+number, +count));
+//     let number = parseFloat(prompt("Enter the number:"));
+//     let count = parseInt(prompt("Enter the number of additions (count):"));
+//     alert(three(+number, +count));
   
-function KmToM(km) {
-    return km * 1000;
-  }
+// function KmToM(km) {
+//     return km * 1000;
+//   }
   
-  function KmToCm(km) {
-    return km * 100000;
-  }
+//   function KmToCm(km) {
+//     return km * 100000;
+//   }
   
-  function getMetric(metric, km) {
-    if (metric === "m") {
-      return KmToM(km);
-    } else if (metric === "cm") {
-      return KmToCm(km);
-    } else {
-      return "Unknown unit of measurement";
-    }
-  }
+//   function getMetric(metric, km) {
+//     if (metric === "m") {
+//       return KmToM(km);
+//     } else if (metric === "cm") {
+//       return KmToCm(km);
+//     } else {
+//       return "Unknown unit of measurement";
+//     }
+//   }
   
-  let metric = prompt("Enter the unit of measurement (m or cm):");
-  let km = parseFloat(prompt("Enter the distance in kilometers:"));
+//   let metric = prompt("Enter the unit of measurement (m or cm):");
+//   let km = parseFloat(prompt("Enter the distance in kilometers:"));
   
-  if (typeof result === "number") {
-    alert(`Distance in the specified unit: ${result}`);
-  } else {
-    alert(getMetric(metric, km));;
-  }
-  
+//   if (typeof result === "number") {
+//     alert(`Distance in the specified unit: ${result}`);
+//   } else {
+//     alert(getMetric(metric, km));;
+//   }
 
+// Lesson 22 Об'єкти
+// let car = {
+//     model: "Mazda",
+//     year: 2019,
+//     color: "red",
+//     type: "electric",
+//     signal: function() {
+//       alert('fa! fa!');
+//     }
+// };
+  
+//   car.color = "blue";
+  
+//   car.signal();
 
+// let salaries = {
+//     frontend: 12000,
+//     backend: 10000,
+//     designer: 8000,
+//     dayPay() {
+//       alert('We must pay salary on Tuesday!');
+//     },
+//     total() {
+//       let sum = 0;
+//       for (let key in this) {
+//         if (typeof this[key] === 'number') {
+//           sum += this[key];
+//         }
+//       }
+//       console.log(sum);
+//     }
+//   };
+  
+//   salaries.total();
+//   salaries.manager = 5000;
+//   salaries.total();
+
+// function Laptop(brand, system, cost) {
+//     this.brand = brand;
+//     this.system = system;
+//     this.cost = cost;
+//   }
+  
+//   let dell = new Laptop('Dell', 'Windows', 800);
+//   let apple = new Laptop('Apple', 'MAC OS', 1700);
+  
+//   console.log(dell);
+//   console.log(apple);
+  
+// function Laptop(brand, system, cost) {
+//     this.brand = brand;
+//     this.system = system;
+//     this.cost = cost;
+  
+//     this.valueOf = function () {
+//       return this.cost;
+//     };
+  
+//     this.toString = function () {
+//       return `${this.brand} ${this.system} ${this.cost}`;
+//     };
+  
+//     this[Symbol.toPrimitive] = function(hint) {
+//       if (hint === 'string') {
+//         return this.brand;
+//       } else if (hint === 'number') {
+//         return this.cost;
+//       } else {
+//         return `${this.brand} ${this.system} ${this.cost} // `;
+//       }
+//     };
+//   }
+  
+//   let dell = new Laptop('Dell', 'Windows', 800);
+//   let apple = new Laptop('Apple', 'MAC OS', 1700);
+  
+//   console.log(String(dell));
+//   console.log(+apple);
+//   console.log(dell + apple);
+  
+  
+  
+  
   
   
 
